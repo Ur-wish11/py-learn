f077836dc37d5e0fbd9c98090f9eccc342c6d95a
Ur-Wish <56624510+Ur-wish11@users.noreply.github.com>
2020-10-03 02:49:57 +0530

    Update changes to Eaten
    The code is running now. Congratulations

--- eatwhileloop.py
+++ eatwhileloop.py
@@ -1,11 +1,8 @@
 import time
-
-print "you will be ...."
-
-count = 8
-while (0< count < 9):
-    print count
+count = 10
+while (0< count < 11):
+    print(count)
     count = count - 1
     time.sleep(1)
-
-print "eaten!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+else:
+    print("Burp...I have eaten !")
